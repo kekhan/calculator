4ca89c37f200a29f9160d462348b32291adb1675
Kerin Khan <kerink585@gmail.com>
2017-11-07 20:02:33 -0800

    fix the equal button

---
+++ calculator.js
@@ -0,0 +1,26 @@
+function main(button){
+	// the eval() method evaluates string of integers
+	var x = button.value;
+
+	var y= document.getElementById('screen').innerHTML += x;
+	if (button.value==="="){
+		equal(y);
+	}
+	else if(button.value==="C"){
+		clear(y);
+	}
+
+
+}
+
+function clear(str){
+	var clear =0
+	document.getElementById('screen').Location.reload() ;
+
+}
+function equal(str){
+	var newStr=str.replace("=","");
+	
+	console.log(newStr)
+    document.getElementById('screen').innerHTML=eval(newStr);
+}
