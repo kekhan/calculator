99529f5e5d44c1aefe3a15af3cd17fbfda96ef1e
Kerin Khan <kerink585@gmail.com>
2017-11-09 10:50:28 -0800

    added audio tagfor button clicks

--- calculator.js
+++ calculator.js
@@ -1,8 +1,13 @@
 function main(button){
 	// the eval() method evaluates string of integers
+	var sounds = document.getElementById("sound");
+	sounds.play();
 	var x = button.value;
 
 	var y= document.getElementById('screen').innerHTML += x;
+	if(y.length>30){
+		document.getElementById('screen').innerHTML ="Input Limit reached";
+	}
 	if (button.value==="="){
 		equal(y);
 	}
